b585d3367bcf20b7c53b279e2dcb15050c1bbea3
Jakob Dam Jensen <jakob@nopointlessapps.com>
2017-06-28 22:41:46 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Intercom.Mixfile do
 
   def project do
     [app: :intercom,
-     version: "0.0.2",
-     elixir: "~> 1.2",
+     version: "0.0.3",
+     elixir: "~> 1.4.4",
      description: description(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
