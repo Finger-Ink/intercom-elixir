822bcbc5dfb7da41cf929e319b2cd7ddc53c744e
Brendan Kilfoil <brendan.kilfoil@finger-ink.com>
2019-07-09 21:09:42 +1200

    Fix deprecation warning

--- lib/intercom/client.ex
+++ lib/intercom/client.ex
@@ -2,10 +2,11 @@ defmodule Intercom.Client do
   use HTTPoison.Base
 
   def process_request_headers(headers) do
-    Enum.into(headers, [
+    headers
+    |> Keyword.merge(
       Accept: "application/json",
       "Content-Type": "application/json"
-    ])
+    )
   end
 
   def process_url(url) do
